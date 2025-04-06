cc22eedb87446653ab3ea35d304327524af2f709
Siam <siam48444@gmail.com>
2025-04-06 11:55:48 +0600

    siam

---
+++ index.js
@@ -0,0 +1,5 @@
+const gameBoard = document.getElementById('gameBoard');
+const topSection = document.getElementById('topSection');
+const topSection = document.getElementById('topSection');
+const highScoreText = document.getElementById('highScoreText');
+const currentScoreText = document.getElementById('currentScoreText');
