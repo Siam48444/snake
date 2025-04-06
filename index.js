b031a4e2d18410c66a973c453e3cfaf1928f5170
Siam <siam48444@gmail.com>
2025-04-06 19:38:00 +0600

    siam

--- index.js
+++ index.js
@@ -10,8 +10,7 @@ const currentScoreText = document.getElementById('currentScoreText');
 let boardWidth;
 let boardHeight;
 let cellWidth;
-let rows;
-let columns;
+let cellCount = 20;
 
 
 // Set the board size when the page loads or the window is resized
@@ -19,11 +18,13 @@ window.addEventListener('load', setGameBoard);
 window.addEventListener('resize', setGameBoard);
 
 
-// Calculates and sets the game board size based on the window height
+// Calculates and sets the game board size
 function setGameBoard() {
 	boardHeight = Math.min(window.innerHeight, window.innerWidth) * 0.9 - topSection.offsetHeight;
 	boardWidth = boardHeight; // Make the board square
 
 	gameBoard.height = boardHeight; // Set the board height and width
 	gameBoard.width = boardWidth; 
+
+	cellWidth = boardHeight / cellCount; // Calculate cell width
 }
