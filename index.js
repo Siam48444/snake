21b18a8173b9d6e4042c02567867d400801c871f
Siam <siam48444@gmail.com>
2025-04-06 12:09:50 +0600

    siam

--- index.js
+++ index.js
@@ -1,14 +1,12 @@
 const gameBoard = document.getElementById('gameBoard');
 const contex = gameBoard.getContext('2d');
 const topSection = document.getElementById('topSection');
-const topSection = document.getElementById('topSection');
 const highScoreText = document.getElementById('highScoreText');
 const currentScoreText = document.getElementById('currentScoreText');
 
 
 // board
-let boardHeight;
-let boardWidth;
+let boardSize;
 let cellWidth;
 let rows;
 let columns;
@@ -20,5 +18,6 @@ window.addEventListener('resize', setBoard);
 
 
 function setBoard() {
-
+	boardSize = window.innerHeight * 0.9 - topSection.offsetHeight;
+	gameBoard.height = gameBoard.width = boardSize;
 }
