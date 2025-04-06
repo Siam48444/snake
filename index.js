a08b9227e4a5ceea214928c8ebc56806a195ba07
Siam <siam48444@gmail.com>
2025-04-06 12:02:10 +0600

    siam

--- index.js
+++ index.js
@@ -1,5 +1,24 @@
 const gameBoard = document.getElementById('gameBoard');
+const contex = gameBoard.getContext('2d');
 const topSection = document.getElementById('topSection');
 const topSection = document.getElementById('topSection');
 const highScoreText = document.getElementById('highScoreText');
 const currentScoreText = document.getElementById('currentScoreText');
+
+
+// board
+let boardHeight;
+let boardWidth;
+let cellWidth;
+let rows;
+let columns;
+
+
+// set the board size
+window.addEventListener('load', setBoard);
+window.addEventListener('resize', setBoard);
+
+
+function setBoard() {
+
+}
