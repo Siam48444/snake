90c31d1a8114ccfe711b3abef062ede9f609706b
Siam <siam48444@gmail.com>
2025-04-12 11:43:50 +0600

    siam

--- assets/sounds/sounds.js
+++ assets/sounds/sounds.js
@@ -4,6 +4,6 @@ export const eatSound = new Audio('./assets/sounds/eat.mp3');
 
 // Plays the sound
 export function playSound(sound) {
-	sound.currenTime = 0;
+	sound.currentTime = 0;
 	sound.play();
 }
